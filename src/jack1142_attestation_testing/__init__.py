7d8043da41bdcc532130ed770db784d57a86b314
Jakub Kuczys <me@jacken.men>
2026-04-04 08:43:04 +0200

    Initial commit

---
+++ src/jack1142_attestation_testing/__init__.py
@@ -0,0 +1,14 @@
+import sys
+
+from pypi_attestations import Attestation
+from sigstore.models import Bundle
+
+
+def main() -> None:
+    bundle_file = sys.argv[1]
+    with open(bundle_file, "rb") as fp:
+        sigstore_bundle = Bundle.from_json(fp.read())
+    attestation = Attestation.from_bundle(sigstore_bundle)
+    for filename in sys.argv[2:]:
+        with open(f"{filename}.publish.attestation", "w") as fp:
+            fp.write(attestation.model_dump_json())
